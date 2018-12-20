7182d62f83e9282af92a90bda585d71dbc02bff0
esfox <osairis100@gmail.com>
2018-12-20 12:31:50 +0800

    Changed prefix

--- bot.js
+++ bot.js
@@ -21,9 +21,9 @@ bot.login(args)
     if(error) console.log("gitgud haha"); 
 });
 
-var ping = "<@247955535620472844>•d";
+var ping = "<@247955535620472844>•";
 
-const prefix = "'";
+const prefix = ";";
 
 bot.on('ready', () =>
 {
