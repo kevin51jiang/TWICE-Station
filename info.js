64466ffb2d19522fc8ae16a67d202c7b3c9faaea
esfox <osairis100@gmail.com>
2019-02-26 23:14:01 +0800

    Fixed unfollow command

--- info.js
+++ info.js
@@ -218,11 +218,7 @@ exports.unfollow = async (message) =>
         return message.reply(`you haven't followed ${channels.length > 1?
             'those channels' : 'that channel'}.`);
     
-    follows.forEach((f, i) =>
-    {
-        if(channels.includes(f)) follows.splice(i, 1);
-    });
-    
+    follows = follows.filter(f => channels.includes(f));
     database.updateFollows(message.author.id, JSON.stringify(follows))
         .then(_ => respond());
 
